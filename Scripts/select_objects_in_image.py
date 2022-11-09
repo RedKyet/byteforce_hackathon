358697c81c08da7606f248cd6c01590a74bb8af1
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2022-11-09 19:48:20 +0200

    tweaks for object selection

--- Scripts/select_objects_in_image.py
+++ Scripts/select_objects_in_image.py
@@ -1,20 +1,15 @@
 import cv2
 # read image without color
 image = cv2.imread('Assets\\Greyscale test\\start.png')
-"""cv2.imshow('Greyscaled',img)
-cv2.waitKey(0)"""
-
 original = image.copy()
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+gray = cv2.imread('Assets\\Greyscale test\\start.png',0)
 blur = cv2.GaussianBlur(gray, (5,5), 0)
-thresh = cv2.threshold(blur, 245, 255, cv2.THRESH_BINARY_INV)[1]
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
+thresh = cv2.threshold(blur, 230, 255, cv2.THRESH_BINARY_INV)[1]
+#kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
 #masked = cv2.bitwise_and(original, original, mask=thresh)
 #dilate = cv2.dilate(thresh, kernel, iterations=1)
 
-# Find contours, obtain bounding box coordinates, and extract ROI
-#
-#
 cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 image_number = 0

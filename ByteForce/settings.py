de987d726458ef6160e14e4b46adad909210ad62
AndyGooShooter <91827827+AndyGooShooter@users.noreply.github.com>
2022-11-09 19:21:25 +0200

    Final: Prototype for File Recieval
    Moved destination from 'media' to 'Website/static';
    Can now only process one file at a time;
    File now has custom name (currently 'cache');

--- ByteForce/settings.py
+++ ByteForce/settings.py
@@ -126,5 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # media directory in the root directory
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Website/static') # media directory in the root directory
+MEDIA_URL = '/Website/static/'
--- Website/views.py
+++ Website/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
-#from Website.static import placeholder # temporary
+from django.core.files.storage import FileSystemStorage
+import json
+import os
 
 def home(request):
     return render(request, 'main.html')
-from django.core.files.storage import FileSystemStorage
+
 
 def upload(request):
-    import json
-    print(request.FILES['document'])
+    
+    # print(request.FILES['document'])
+    
     uploaded_file = request.FILES['document']
     fs = FileSystemStorage()
-    name = fs.save(uploaded_file.name, uploaded_file)
-    print(fs.url(name))
+    
+    fs.delete('cache.jpg')
+    
+    name = fs.save('cache.jpg', uploaded_file)
+    
+    # print(fs.url(name))
 
-    #uploaded_file = request.FILES
-    #fs = FileSystemStorage()
-    #name = fs.save(uploaded_file.name, uploaded_file)
-    #context['url'] = fs.url(name)
     return render(request, 'main.html')

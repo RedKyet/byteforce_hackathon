6edcff2ba426116f0b5caa92f9a8cda45e430128
AndyGooShooter <91827827+AndyGooShooter@users.noreply.github.com>
2022-11-09 18:01:15 +0200

    test1
    showing an image from static folder

--- Website/views.py
+++ Website/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from Website.static import placeholder # temporary
 
-# Create your views here.
+def home(request):
+    
+   
+    return render(request, {placeholder})

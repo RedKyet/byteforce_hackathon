9c13e94d0b50cdb4101ada8084b1706d6536ca12
Smartyguy001 <rares.neculau@yahoo.com>
2022-11-09 18:48:39 +0200

    idk something contrast

--- extract_imagine.py
+++ extract_imagine.py
@@ -3,11 +3,22 @@ import math
 import time
 import os
 
-imgname = input("Enter image name: ")
+imgname = "fotografietest.png"
 
-img = cv.imread(imgname, cv.IMREAD_GRAYSCALE)
+img = cv.imread(imgname, cv.IMREAD_COLOR)
 rows, cols, _ = img.shape
+grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+ret, invbinaryimg = cv.threshold(grayimg, 230, 255, cv.THRESH_BINARY_INV)
 
-cv.imshow(img)
+contrastarr = [[0 for j in range(cols)] for i in range(rows)]
+
+for i in range(rows):
+    for j in range(cols):
+        if (i > 0):
+            contrastarr = max(contrastarr, abs(grayimg[i][j] - gray[i-1][j]))
+
+cv.imshow("grayimg", grayimg)
+cv.imshow("invbinaryimg", invbinaryimg)
+    
 cv.waitKey(0)
 cv.destroyAllWindows()

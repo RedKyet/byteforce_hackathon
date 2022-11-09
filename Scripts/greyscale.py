c135cfaea57b6eebbdab8322c8fb6ed56b378a9b
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2022-11-09 17:11:19 +0200

    fixed greyscale test in comment

--- Scripts/greyscale.py
+++ Scripts/greyscale.py
@@ -1,5 +1,5 @@
 import cv2
 # read image without color
 img = cv2.imread('Assets\\Greyscale test\\rock.jpeg',0)
-cv2.imshow('Greyscaled',img)
-cv2.waitKey(0)
+"""cv2.imshow('Greyscaled',img)
+cv2.waitKey(0)"""

b7532bd8050cb0e9749704509c2fb55825f091cb
Smartyguy001 <rares.neculau@yahoo.com>
2022-11-09 17:54:19 +0200

    non essential

--- extract_imagine.py
+++ extract_imagine.py
@@ -3,3 +3,11 @@ import math
 import time
 import os
 
+imgname = input("Enter image name: ")
+
+img = cv.imread(imgname, cv.IMREAD_GRAYSCALE)
+rows, cols, _ = img.shape
+
+cv.imshow(img)
+cv.waitKey(0)
+cv.destroyAllWindows()

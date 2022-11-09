1d5572a0a383542f32de1ae1e55da787b6088a89
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2022-11-09 17:23:30 +0200

    script now saved the greyscaled image as a file

--- Scripts/greyscale.py
+++ Scripts/greyscale.py
@@ -3,3 +3,4 @@ import cv2
 img = cv2.imread('Assets\\Greyscale test\\rock.jpeg',0)
 """cv2.imshow('Greyscaled',img)
 cv2.waitKey(0)"""
+cv2.imwrite('Assets\\Greyscale test\\rock_greyscaled.jpg',img)

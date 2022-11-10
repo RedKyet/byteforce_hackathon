5830e68f6966240869dbf9accbd4d1c5345b239c
Andrei <mafteiandreiiulian@gmail.com>
2022-11-10 08:06:13 +0200

    am adaugat la cod de adaugare numar

--- Scripts/select_objects_in_image.py
+++ Scripts/select_objects_in_image.py
@@ -1,4 +1,6 @@
 import cv2
+from PIL import Image, ImageFont, ImageDraw
+
 # read image without color
 image = cv2.imread('Assets\\Greyscale test\\start.png')
 """cv2.imshow('Greyscaled',img)
@@ -12,8 +14,22 @@ kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
 #masked = cv
 # 2.bitwise_and(original, original, mask=thresh)
 #dilate = cv2.dilate(thresh, kernel, iterations=1)
+#puts number on image
+im1 = Image.open('ROI_0.png')
+ac = Image.new('L', (im1.width, im1.height))
 
-# Find contours, obtain bounding box coordinates, and extract ROI
+
+def adaugare_numar(a, num):
+    title_font = ImageFont.truetype('Scripts\\fontu.ttf', 25)
+    title_text = num
+    image_editable = ImageDraw.Draw(a)
+    image_editable.text([15, 15], title_text, fill="red", font=title_font)
+    #ImageDraw.Draw.text(xy, text, fill=None, font=None, anchor=None, spacing=0, align=”left”)
+    im1.save("result.png")
+
+## 
+# 
+# # Find contours, obtain bounding box coordinates, and extract ROI
 #
 #
 cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -25,6 +41,8 @@ for c in cnts:
     ROI = original[y:y+h, x:x+w]
     cv2.imwrite("Assets\\Objects\\ROI_{}.png".format(image_number), ROI)
     image_number += 1
+
+
 #
 #
 #
